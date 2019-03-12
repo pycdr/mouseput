82004d40095e8088e7a66da356994a14ed1a5954
pycdr <44697997+pycdr@users.noreply.github.com>
2019-03-12 16:15:32 +0330

    - upload main files
    uploaded files: [hacker.py , target.py]

---
+++ hacker.py
@@ -0,0 +1,55 @@
+from socket import socket
+#==========================#
+s = socket()
+host = input("~ target ip: ")
+port = int(input("~ target port: "))
+s.connect((host,port))
+#==========================#
+while 1:
+	t = input(
+"""[
+	1) control with moving your mouse :)
+	2) get input and send position
+] which one[1,2]? """)
+	if t not in {"1","2"}:
+		print("~ just type 1 or 2.")
+		continue
+	break
+#==========================#
+print("~ start...")
+try:
+	if t == "1":
+		import pyautogui as g
+		while 1:
+			pos = g.position()
+			d = str(pos[0])+","+str(pos[1])
+			s.sendall(d)
+			d = s.recv(1024)
+			d = d.decode("utf8")
+			if d == "0":
+				print("~ error in set target mouse position!")
+				if input("~ continue[y=yes/other=no]?") == "y":
+					continue
+				break
+			elif d == "1":
+				print("~ fine!")
+		s.close()
+	else:
+		while 1:
+			print("pos:")
+			d = input("\tx:")+","+input("\ty:")
+			s.sendall(d.encode("utf8"))
+			d = s.recv(1024)
+			d = d.decode("utf8")
+			if d == "0":
+				print("~ error in set target mouse position!")
+				if input("~ continue?") == "y":
+					continue
+				break
+			elif d == "1":
+				print("~ fine!")
+
+except KeyboardInterrupt:
+	print("~ by..!")
+finally:
+	s.close()
---
+++ target.py
@@ -0,0 +1,27 @@
+import socket,os,sys
+import pyautogui as gui
+s = socket.socket()
+def get_ip():
+	if sys.platform in ("linux","darwin"):
+		return os.popen("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'").read()
+	#elif sys.platform in ("win32","cygwin"):
+	else:
+		si = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		si.connect(("8.8.8.8", 80))
+		si.close()
+		return s.getsockname()[0]
+s.bind((get_ip(),6969))
+s.listen(1)
+h = s.accept()
+while 1:
+	d = h[0].recv(1024)
+	d = str(d)[2:-1].split(",")
+	if len(d) != 2:
+		h[0].sendall(b"0")
+		break
+	try:
+		gui.moveTo(int(d[0]),int(d[1]))
+		h[0].sendall(b"1")
+	except Exception as err:
+		h[0].sendall(b"0")
+s.close()
